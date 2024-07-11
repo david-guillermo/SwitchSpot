819890856faec3bbe61607db3fe8554976f59d32
david-guillermo <davicraft.yt15@gmail.com>
2024-07-11 13:41:03 -0400

    SwitchSpot

--- docs/Scripts/Login.js
+++ docs/Scripts/Login.js
@@ -3,9 +3,18 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
-container.classList.add("sign-up-mode");
+    container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-container.classList.remove("sign-up-mode");
+    container.classList.remove("sign-up-mode");
 });
+
+let email = "";
+const emailRegex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9._]+\.[a-zA-Z]+$/;
+
+if (emailRegex.test(email)) {
+    console.log("El correo electrónico es válido.");
+}else{
+    console.log("El correo electrónico no es válido.");
+};
